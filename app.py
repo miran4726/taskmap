e119d100f138edbbe1acc918383db4303899c15d
miran4726 <dervismirankurtbogan25@gmail.com>
2026-08-21 10:01:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return "<h1 style='text-align:center; margin-top:50px; font-family:sans-serif;'>Giriş Yap Sayfası Yapım Aşamasında 🚀</h1>"
+
+@app.route('/register')
+def register():
+    return "<h1 style='text-align:center; margin-top:50px; font-family:sans-serif;'>Üye Ol / Kayıt Sayfası Yapım Aşamasında 🚀</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
